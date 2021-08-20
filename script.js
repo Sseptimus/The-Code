c586a43aa081eb63978ec2ffe758b1a2ecd48449
ollielynas <49426014+ollielynas@users.noreply.github.com>
2021-08-20 23:21:42 +1200

    added light mode
    its not very mogulure, every time you create a new element you have to add both colors schemes  to the script.js file. i going to try to fix that later

--- script.js
+++ script.js
@@ -10,18 +10,24 @@ function switchToStats() {
         console.log("pressed button");
         document.getElementById("statButton").innerHTML = "Paramaters";
         document.getElementById("parameters").style.display = "none";
-        document.getElementById("options1").style.color = "orange";
-        document.getElementById("statButton").style.color = "rgb(58, 179, 166)";
         document.getElementById("stats").style.display = "grid";
-  } else {
+        if (eyepain == "none"){
+            document.getElementById("options1").style.color = "orange";
+            document.getElementById("statButton").style.color = "rgb(58, 179, 166)";
+            
+        }
+  }else{
         state = 0;
         document.getElementById("parameters").style.display = "grid";
         document.getElementById("stats").style.display = "none";
         document.getElementById("statButton").innerHTML = "Stats";
         document.getElementById("region").innerHTML = "Region";
         document.getElementById("population").innerHTML = "Undefinded Inhabitants";
-        document.getElementById("options1").style.color = "rgb(58, 179, 166)";
-        document.getElementById("statButton").style.color = "orange";
+        if (eyepain == "none"){
+            document.getElementById("options1").style.color = "rgb(58, 179, 166)";
+            document.getElementById("statButton").style.color = "orange";
+            document.getElementById("options1").style.color = "rgb(58, 179, 166)";
+        }
     }
 }
 
@@ -41,10 +47,40 @@ function zooming(e) {
     d3.select("svg g").attr("transform", e.transform);
 }
 
+function lightToDark(){
+    if (eyepain == "none"){
+        eyepain = "ungodly"
+
+        document.getElementById("main").style.backgroundColor = "#add8e6";
+        document.getElementById("lightVsDark").innerHTML = "&#9728;&nbsp;&nbsp;&nbsp;&nbsp;";
+        document.getElementById("lightVsDark").style.color = "orange";
+        document.getElementById("lightVsDark").style.backgroundColor = "white";
+        document.getElementById("options1").style.backgroundColor = "white";
+        document.getElementById("options1").style.color = "black";
+        document.getElementById("statButton").style.backgroundColor = "#C9C9C9";
+        document.getElementById("statButton").style.color = "black";
+        document.getElementById("nzmap").style.fill = "	#98FB98";
+
+    }else{
+        eyepain = "none"
+        document.getElementById("lightVsDark").innerHTML = "&nbsp;&nbsp;&nbsp;&#127769;";
+        document.getElementById("lightVsDark").style.color = "purple";
+        document.getElementById("lightVsDark").style.backgroundColor = "black";
+        document.getElementById("options1").style.backgroundColor = "#282b2e";
+        document.getElementById("options1").style.color = "rgb(58, 179, 166)";
+        document.getElementById("statButton").style.backgroundColor = "rgb(58, 49, 49)";
+        document.getElementById("statButton").style.color = "orange";
+        document.getElementById("main").style.backgroundColor = "#2f3336";
+        document.getElementById("nzmap").style.fill = "#52585d";
+
+    }
+}
+
 // variabales
 var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 let zoom = d3.zoom().on("zoom", zooming);
 var state = 0;
+var eyepain = "none"
 
 //testing is mobile
 if (isMobile) {// tests of the website is in mobile
@@ -81,5 +117,4 @@ d3.select("svg")
         document.getElementById("population").innerHTML = populationsOfRegions[name] + " Inhabitants";
     });
 });
-
 
