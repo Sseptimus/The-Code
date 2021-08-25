fd3da56d2439c61e04bcc09a75f4911d273a6145
ollielynas <49426014+ollielynas@users.noreply.github.com>
2021-08-25 15:05:59 +1200

    made them hollow
    i think it looks better but idk

--- res/snap.js
+++ res/snap.js
@@ -1,17 +1,25 @@
 var s = Snap("#svgStartIcon")
 var triangle1 = s.polygon([1,1, 1,9, 7,5])
-var square1 = [1,1, 1,9, 2,9]
+var square1 = [0,1, 0,9, 1,9]
 var trianglePTS = [1,1, 1,9, 7,5]
-triangle1.attr("fill", "black");
+triangle1.attr("stroke-width","0.5")
+triangle1.attr("stroke","black")
+triangle1.attr("fill", "transparent");
 var triangle2 = s.polygon([1,1, 1,9, 7,5])
-var square2 = [1,1, 2,9, 2,1]
-triangle2.attr("fill", "black");
+var square2 = [0,1, 1,9, 1,1]
+triangle2.attr("stroke-width","0.5")
+triangle2.attr("stroke","black")
+triangle2.attr("fill", "transparent");
 var triangle3 = s.polygon([1,1, 1,9, 7,5])
 var square3 = [6,1, 6,9, 7,9]
-triangle3.attr("fill", "black");
+triangle3.attr("stroke-width","0.5")
+triangle3.attr("stroke","black")
+triangle3.attr("fill", "transparent");
 var triangle4 = s.polygon([1,1, 1,9, 7,5])
 var square4 = [6,1, 7,9, 7,1]
-triangle4.attr("fill", "black");
+triangle4.attr("fill", "transparent");
+triangle4.attr("stroke-width","0.5")
+triangle4.attr("stroke","black")
 
 
 
@@ -32,9 +40,9 @@ function animatePause(){
 
 /* 
 
-<polygon id="triangle1" points="1,1 1,9 7,5" style="fill:black;stroke:black;stroke-width:.1" />
-<polygon id="triangle2" points="1,1 1,9 7,5" style="fill:black;stroke:black;stroke-width:.1" />
-<polygon id="triangle3" points="1,1 1,9 7,5" style="fill:black;stroke:black;stroke-width:.1" />
-<polygon id="triangle4" points="1,1 1,9 7,5" style="fill:black;stroke:black;stroke-width:.1" />
+<polygon id="triangle1" points="1,1 1,9 7,5" style="fill:transparent;stroke:transparent;stroke-width:.1" />
+<polygon id="triangle2" points="1,1 1,9 7,5" style="fill:transparent;stroke:transparent;stroke-width:.1" />
+<polygon id="triangle3" points="1,1 1,9 7,5" style="fill:transparent;stroke:transparent;stroke-width:.1" />
+<polygon id="triangle4" points="1,1 1,9 7,5" style="fill:transparent;stroke:transparent;stroke-width:.1" />
 
 */
