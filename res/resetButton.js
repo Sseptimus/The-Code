34987a883b2c679a64895ab3470c8620624cdbae
ollielynas <49426014+ollielynas@users.noreply.github.com>
2021-09-14 12:47:57 +1200

    trying to triangle to button

---
+++ res/resetButton.js
@@ -0,0 +1,6 @@
+var s3 = Snap("#resetButtonSvg")
+
+var triangle4 = s3.polygon([1,1, 1,9, 7,5])
+triangle4.attr("fill", "transparent");
+triangle4.attr("stroke-width","0.5")
+triangle4.attr("stroke","black")
